3c540937991b273c87967eca4f2b25b61027210b
ChristopherOaks <113634963+ChristopherOaks@users.noreply.github.com>
2024-02-28 14:58:52 -0500

    Added the Files for MHS Group Project

---
+++ MHSGroupProject/MHSGroupProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MHSGroupProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
